81ca8671db9bb0c6c4d66e8c8af23868dd4c1143
Farbod Moayeri <fmoayeri2@myseneca.ca>
2025-09-05 15:36:43 -0400

    Fixed logger.js and got app running

---
+++ src/logger.js
@@ -0,0 +1,23 @@
+// src/logger.js
+require('dotenv').config();
+const pino = require('pino');
+
+// Use `info` as our standard log level if not specified
+const options = { level: process.env.LOG_LEVEL || 'info' };
+
+// If we're doing `debug` logging, make the logs easier to read
+if (options.level === 'debug') {
+  // https://github.com/pinojs/pino-pretty
+  options.transport = {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  };
+}
+
+const logger = pino(options);
+
+// Create and export a Pino Logger instance:
+// https://getpino.io/#/docs/api?id=logger
+module.exports = logger;
