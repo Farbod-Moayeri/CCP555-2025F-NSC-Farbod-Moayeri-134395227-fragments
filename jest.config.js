a267b7a92d922b1aa64cc2f03a832ffe2a42bbad
Farbod Moayeri <fmoayeri2@myseneca.ca>
2025-09-23 14:52:04 -0400

    Fixed ESlint and jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
+// jest.config.js
+
+const path = require('path');
+const dotenv = require('dotenv');
+
+// Load env.jest instead of .env/debug.env
+dotenv.config({ path: path.join(__dirname, 'env.jest') });
+
 /** @type {import('jest').Config} */
 const config = {
+  testEnvironment: 'node',
   testMatch: ['**/tests/unit/**/*.test.js'],
 };
 
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -8,8 +8,15 @@ const express = require('express');
 // Create a router on which to mount our API endpoints
 const router = express.Router();
 
-// Define our first route, which will be: GET /v1/fragments
-router.get('/fragments', require('./get'));
+// Import your authentication function/middleware
+const { authenticate } = require('../../auth'); // NOTE: The path may be different for you
+
+// Apply the authenticate() middleware before the main route handler.
+router.get('/fragments', authenticate(), (req, res) => {
+  // This code will now only run for authenticated requests.
+  // ... your logic to get and return fragments for the authenticated user
+  res.status(200).json({ status: 'ok', fragments: [] });
+});
 // Other routes (POST, DELETE, etc.) will go here later on...
 
 module.exports = router;
